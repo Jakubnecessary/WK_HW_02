c1ec71a23fd1c3be99989232c3660165b6453ce6
jakubnecessary <cabbage1991@gmail.com>
2021-06-23 14:08:24 +0100

    customer has age/tested

--- tests/customer_test.py
+++ tests/customer_test.py
@@ -5,12 +5,15 @@ from src.drink import *
 
 class TestCustomer(unittest.TestCase):
     def setUp(self):
-        self.customer = Customer("Nathan", 100)
+        self.customer = Customer("Nathan", 100, 20)
         self.drink = Drink("Absinth", 30)
         self.pub = Pub("Starbar", 0)
 
     def test_customer_has_name(self):
         self.assertEqual("Nathan", self.customer.name)
+
+    def test_customer_age(self):
+        self.assertEqual(20, self.customer.age)
 
     def test_customer_reduce_wallet(self):
         self.customer.reduce_wallet(10)
@@ -20,3 +23,4 @@ class TestCustomer(unittest.TestCase):
         self.customer.reduce_wallet(self.drink.price)
         self.pub.increase_counter(self.drink.price)
         self.assertEqual(70, self.customer.wallet)
+        self.assertEqual(30, self.pub.counter)
