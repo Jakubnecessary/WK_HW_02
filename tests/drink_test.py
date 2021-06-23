0639e062d2fb76abd89b90624327b018c03d4221
jakubnecessary <cabbage1991@gmail.com>
2021-06-23 11:32:12 +0100

    drink class setted up

--- tests/drink_test.py
+++ tests/drink_test.py
@@ -7,3 +7,6 @@ class TestDrink(unittest.TestCase):
 
     def test_drink_has_name(self):
         self.assertEqual("Absinth", self.drink.name)
+
+    def test_drink_has_price(self):
+        self.assertEqual(30, self.drink.price)
